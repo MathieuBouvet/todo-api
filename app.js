55c6b3002f2eef33a39dd918a65d239156a8cf3e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-20 22:43:46 +0200

    Start todo-api project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const corsPolicy = require("./middlewares/corsPolicy");
+
+const test = require("./routes/todo");
+
+const app = express();
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
+    console.error(error);
+  });
+
+app.use(corsPolicy);
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World ;)");
+});
+
+app.use("/test", test);
+
+module.exports = app;
