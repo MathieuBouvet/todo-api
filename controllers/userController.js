cb3265bf07fe1944f6e0947435c5b54d40b738a1
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-21 23:41:55 +0200

    Use findById instead of findOne in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,7 @@
 const User = require("../models/user");
 
 exports.getUser = (req, res) => {
-  User.findOne({
-    _id: req.params.id,
-  })
+  User.findById(req.params.id)
     .then(userFound => {
       res.status(200).json(userFound);
     })
