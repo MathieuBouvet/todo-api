cc421172862a039549a40e55af104f9c11335a6a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-22 21:42:38 +0200

    Register the post user route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
+const bodyParser = require("body-parser");
 
 router.get("/:id", userController.getUser);
+router.post("/", bodyParser.json(), userController.addUser);
+
 module.exports = router;
