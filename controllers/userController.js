703b55d659b79b8531b6910605ada9b9feb48d19
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-22 21:43:33 +0200

    Create addUser fonction in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,19 @@ exports.getUser = (req, res) => {
       res.status(404).json(error);
     });
 };
+
+exports.addUser = (req, res) => {
+  const newUser = new User({ ...req.body });
+
+  newUser
+    .save()
+    .then(data => {
+      res.status(201).json({
+        message: "User successfully created",
+        ressource: data,
+      });
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    });
+};
