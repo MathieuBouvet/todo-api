211b0073e1188563056b05500636351529adc206
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-22 21:50:50 +0200

    Register the get all users route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
 
+router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
 router.post("/", bodyParser.json(), userController.addUser);
 
