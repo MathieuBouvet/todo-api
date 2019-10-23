f59b343fa1df598c728fc90457ef709476c9468c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-23 22:26:34 +0200

    Handle a not found user on updateTodo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,6 +3,9 @@ const User = require("../models/user");
 exports.updateTodo = (req, res) => {
   User.findById(req.params.id)
     .then(user => {
+      if (!user) {
+        return Promise.reject("no result");
+      }
       user.todos = req.body;
       return user.save();
     })
@@ -10,6 +13,9 @@ exports.updateTodo = (req, res) => {
       res.status(204).end();
     })
     .catch(error => {
+      if (error === "no result") {
+        res.status(404).json({ message: "ressource not found" });
+      }
       res.status(400).json(error);
     });
 };
