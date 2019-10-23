ef7c3073f80bb6ab46eda9c2847e15b89715ef38
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-23 22:01:50 +0200

    Register todos routes to the user router
    todos is treated as a sub ressource of user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const todosRoutes = require("../routes/todo");
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
 router.post("/", bodyParser.json(), userController.addUser);
+
+router.use("/:id/todos", bodyParser.json(), todosRoutes);
 
 module.exports = router;
