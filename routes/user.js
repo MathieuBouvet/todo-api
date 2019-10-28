afd9af8f2023f8d259dac80a9d3ad68929d66d53
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-28 16:21:07 +0100

    Protect the update todos route with authorship

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const todosRoutes = require("../routes/todo");
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
+const authorship = require("../middlewares/authorship");
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
@@ -9,7 +10,7 @@ router.get("/:id", userController.getUser);
 router.use(bodyParser.json());
 
 router.post("/", userController.addUser);
-router.use("/:id/todos", todosRoutes);
+router.use("/:id/todos", authorship, todosRoutes);
 
 router.post("/login", userController.login);
 
