4506f21a188420b5a2797ec6dd50c41ba3ea6c8b
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-07 12:49:57 +0100

    Fix for OPTIONS requests requiring authentication

--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,7 @@
 const router = require("express").Router({ mergeParams: true });
 const todoCtrl = require("../controllers/todoController");
+const authorship = require("../middlewares/authorship");
 
-router.put("/", todoCtrl.updateTodo);
+router.put("/", authorship, todoCtrl.updateTodo);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const todosRoutes = require("../routes/todo");
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
-const authorship = require("../middlewares/authorship");
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
@@ -10,7 +9,7 @@ router.get("/:id", userController.getUser);
 router.use(bodyParser.json());
 
 router.post("/", userController.addUser);
-router.use("/:id/todos", authorship, todosRoutes);
+router.use("/:id/todos", todosRoutes);
 
 router.post("/login", userController.login);
 
