1ba7e9d55be3b1f21d7a2e85f64e7318218f7c50
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-12-01 16:10:57 +0100

    Protect update todo route from csrf attacks

--- routes/todo.js
+++ routes/todo.js
@@ -1,7 +1,8 @@
 const router = require("express").Router({ mergeParams: true });
 const todoCtrl = require("../controllers/todoController");
 const authorship = require("../middlewares/authorship");
+const csrfProtection = require("../middlewares/csrfProtection");
 
-router.put("/", authorship, todoCtrl.updateTodo);
+router.put("/", authorship, csrfProtection, todoCtrl.updateTodo);
 
 module.exports = router;
