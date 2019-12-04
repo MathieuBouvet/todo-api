50cfdabcf2ab230e5677fdf863339d4d651de903
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-12-04 15:56:02 +0100

    Use passport local strategy to protect login route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const todosRoutes = require("../routes/todo");
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+require("../passport-strategies/local");
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
@@ -11,6 +13,10 @@ router.use(bodyParser.json());
 router.post("/", userController.addUser);
 router.use("/:id/todos", todosRoutes);
 
-router.post("/login", userController.login);
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  userController.passportLogin
+);
 
 module.exports = router;
