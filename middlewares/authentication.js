9154447948eee685f4817ace7a79749bca1d7f1e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-12-05 21:35:41 +0100

    Authenticate requests with passport

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,23 +1,6 @@
-const jwt = require("jsonwebtoken");
-const Cookies = require("cookies");
-
-const getTokenFromCookie = (req, res) => {
-  return new Cookies(req, res).get("access_token");
-};
+const passport = require("passport");
+require("../passport-strategies/jwt");
 
 module.exports = (req, res, next) => {
-  try {
-    const token = getTokenFromCookie(req, res);
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decodedToken.userId) {
-      throw "Invalid token";
-    } else {
-      req.userId = decodedToken.userId;
-      req.csrfTokenFromJwt = decodedToken.csrfToken;
-      next();
-    }
-  } catch (err) {
-    res.header("WWW-Authenticate", "Cookie");
-    res.status(401).json({ error: "Access token is invalid or missing" });
-  }
+  passport.authenticate("jwt", { session: false })(req, res, next);
 };
