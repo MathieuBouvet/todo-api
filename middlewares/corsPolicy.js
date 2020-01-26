5fa5aefb07b8d33c6b6e21d3bffb151ce82bbc13
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-26 22:21:44 +0100

    Add csrf token to cors allowed header

--- middlewares/corsPolicy.js
+++ middlewares/corsPolicy.js
@@ -2,7 +2,7 @@ const corsPolicy = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, x-csrf-token"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
